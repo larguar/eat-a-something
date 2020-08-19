d00af7057378c7f3fdec0a39861c0e64d0e958c6
siminski <laurensiminski@gmail.com>
2020-08-19 18:59:45 -0400

    connection created

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+const sql = require('mysql');
+
+const connection = mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: '',
+	database: 'foodDB'
+});
+
+connection.connect(err => {
+	if (err) {
+		console.error('error connecting: ' + err.stack);
+		return;
+	}
+	console.log('connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
