173a0233c694cae252a9ed563c69fb38e052fc65
siminski <laurensiminski@gmail.com>
2020-08-19 18:42:55 -0400

    schema & seed files created

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS foodDB;
+CREATE DATABASE foodDB;
+
+USE foodDB;
+
+CREATE TABLE foods (
+	id INT AUTO_INCREMENT NOT NULL,
+	restaurant varchar(255) NOT NULL,
+	foodItem varchar(255) NOT NULL,
+	eaten BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO 
+foods (restaurant, foodItem, eaten) 
+
+VALUES 
+('Taco Bell', 'Black Bean Crunchwrap Supreme', false), 
+('Burger King', 'Impossible Whopper', false), 
+('Sprout Natural Choice', 'Chickun Caesar Salad', false), 
+('Taco Bell', 'Black Bean & Rice Burrito', false), 
+('Chipotle', 'Burrito Bowl', false),
+('18-8 Sushi', 'Cucumber Avocado Roll', false);
