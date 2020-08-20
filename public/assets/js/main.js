bf0ee2194f49d881ea1dc20134d0de1ed3c6ba90
siminski <laurensiminski@gmail.com>
2020-08-20 11:07:51 -0400

    conditional statements added to show alert and not submit if both inputs aren't filled out

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -14,16 +14,28 @@ $(function() {
 
   	$('.create-form').on('submit', function(event) {
     	event.preventDefault();
-		const newFood = {
-			restaurant: $('#re').val().trim(), 
-			foodItem: $('#ca').val().trim()
-		};
-		$.ajax('/api/foods', {
-			type: 'POST',
-			data: newFood
-    	}).then(() => {
-			location.reload();
-      	});
+    	let alert = $('.alert');
+    	const restaurant = $('#re').val().trim();
+    	const food = $('#ca').val().trim();
+    	if (!restaurant && !food) {
+	    	alert.html('<i class="far fa-exclamation-circle"></i> Please enter a restaurant and food');
+    	} else if (!restaurant) {
+	    	alert.html('<i class="far fa-exclamation-circle"></i> Please enter a restaurant');
+    	} else if (!food) {
+	    	alert.html('<i class="far fa-exclamation-circle"></i> Please enter a food');
+    	} else {
+	    	alert.html('');
+	    	const newFood = {
+				restaurant: $('#re').val().trim(), 
+				foodItem: $('#ca').val().trim()
+			};
+			$.ajax('/api/foods', {
+				type: 'POST',
+				data: newFood
+	    	}).then(() => {
+				location.reload();
+	      	});
+    	}
   	});
 
 });
