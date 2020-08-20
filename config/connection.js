f61ee514fc5d20d7afb9f664928c9e993378c2b0
siminski <laurensiminski@gmail.com>
2020-08-20 12:57:02 -0400

    changed connection with jawsdb info

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,17 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-	host: 'localhost',
-	port: 3306,
-	user: 'root',
-	password: '',
-	database: 'foodDB'
-});
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+	connection = mysql.createConnection({
+		host: 'localhost',
+		port: 3306,
+		user: 'root',
+		password: '',
+		database: 'foodDB'
+	});
+}
 
-connection.connect((err) => {
-	if (err) {
-		console.error('error connecting: ' + err.stack);
-		return;
-	}
-	console.log('connected as id ' + connection.threadId);
-});
+connection.connect();
 
 module.exports = connection;
